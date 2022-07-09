b4081a4cdce5f902f6d673206bcaad56d8362e36
Cien267 <kiennguyenduc267@gmail.com>
2022-07-09 23:30:46 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="showModal()">SHOW MODAL</button>
+    <Modal 
+    :is-show="isShow"
+    @updateIsShow="updateIsShow"
+  />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      isShow : false
+    }
+  },
+  methods : {
+    showModal(){
+      this.isShow  = true
+    },
+    updateIsShow(value) {
+      this.isShow = value
+    }
   }
 }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="overlay" :class="isShow ? 'slide': 'close'">
+    <div>
+        <h1 class="close-btn" @click="close">
+            × &nbsp;
+        </h1>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    isShow: Boolean
+  },
+  methods: {
+    close() {
+      this.$emit('updateIsShow', false)
+    
+    }
+  },
+}
+</script>
+
+<style scoped> 
+.overlay {
+    width: 50%;
+    height: 0;
+    position: fixed;
+    bottom: 0;
+    left: 25%;
+    z-index: 1;
+    background-color: #add8e6;
+    overflow-y: hidden;
+    transition: 1.0s;
+}
+.close-btn{
+    cursor: pointer;
+}
+.slide {
+  height: 70%;
+}
+.close {
+  height: 0%;
+}
+</style>
